97d097ed4c32f3276b40ffd8f67e8cf64b765766
lacbook <lanuxos@gmail.com>
2023-02-07 17:37:21 +0700

    bugFixed

--- README.md
+++ README.md
@@ -12,8 +12,13 @@ git push --set-upstream origin <branchName>
 ```
 git checkout -b django
 python -m virtualenv venv
-.\venv\Script\activate
+.\venv\Script\activate  # for Windows user, use source ./venv/bin/activate instead for mac user
 python -m pip install django==4.1
+python manage.py migrate
+python manage.py createsuperuser
+python manage.py loaddata fixtures/Footer  # to load data from fixtures file named Footer
+python manage.py runserver
+# visit localhost:8000/matabase/home or localhost:8000/admin
 ```
 ### fastApi
 ### flask
@@ -61,9 +66,17 @@ flask --app lovie --debug run
 
 ## Features
 - User system
-- Add record(s) [only staff permission]
+- Add, update/edit or delete record(s) [only staff permission]
 - Search records
 - List records via status [all, watched, downloaded, deleted]
 
+## bugs
+- links/developer info divide, fetch whole and count then divide by 2 ?
+
 ## Future features
 - Registered could edit tag(s), add pending record(s) and approve via staff
+- plot history statistic graph, store user's changing record [watched/deleted] then count as activity
+- scrapping tag info from mdb, set untag if there is no found info
+- remove [pop()] UNTAG if the len() > 1
+- dataTable on search result for sorting via name/tags/year
+- scrape genes tag from imdb while adding new record, add update btn to fetch on update page
--- django/lovie/matabase/models.py
+++ django/lovie/matabase/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
 
+
 class Matabase(models.Model):
     title = models.CharField(max_length=200, help_text="Enter movies' title")
     year = models.CharField(max_length=4, help_text="Enter released year, eg: xxxx")
@@ -10,18 +11,22 @@ class Matabase(models.Model):
         ('r', 'Removed'),
     )
     status = models.CharField(max_length=10, choices=choices, default='d', help_text="d: Downloaded, w: Watched, r: Removed")
-    
     createdDate = models.DateField(auto_now_add=True)
     updatedDate = models.DateTimeField(auto_now=True)
 
     class Meta:
         ordering = ['-createdDate', 'title']
+        # constraints = [
+        #     models.UniqueConstraint(
+        #         fields=['title', 'year'], name='uniqueDB')
+        # ]
     
     def get_absolute_url(self):
         return reverse('model-detail-view', args=[str(self.id)])
     
     def __str__(self):
         return self.title + " [" + str(self.year) + "]"
+
 
 class Mag(models.Model):
     '''
@@ -49,3 +54,20 @@ class Mag(models.Model):
 
     def __str__(self):
         return f'{self.magReference.title} [{self.mag}]'
+
+
+class Footer(models.Model):
+    footerCategory = [('stack', 'stack'), ('links', 'links'), ('developer', 'developer')]
+    category = models.CharField(max_length=25, choices=footerCategory)
+    title = models.CharField(max_length=25)
+    icon = models.CharField(max_length=100, null=True, blank=True)
+    link = models.URLField(max_length=255, null=True, blank=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['category', 'title', 'link'], name='uniqueFooter')
+        ]
+
+    def __str__(self):
+        return self.title
+    
