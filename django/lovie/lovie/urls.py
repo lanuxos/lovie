5327e94183d4e8ea928095180236c8156ab121bd
La <32545740+lanuxos@users.noreply.github.com>
2024-08-21 14:14:50 +0700

    Update urls.py
    from django branch

--- django/lovie/lovie/urls.py
+++ django/lovie/lovie/urls.py
@@ -19,7 +19,6 @@ from django.contrib.auth import views as auth_views # for login/out
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('matabase/', include('matabase.urls')),
     path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
-    # path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('matabase/', include('matabase.urls')),
 ]
