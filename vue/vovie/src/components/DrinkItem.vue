9453fb74f11153a04d7e4e1132fbd12309a24547
La <32545740+lanuxos@users.noreply.github.com>
2024-10-22 14:53:53 +0700

    Add files via upload

---
+++ vue/vovie/src/components/DrinkItem.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h2>{{ drinkName }}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['drinkName']
+};
+</script>
+
+<style></style>
---
+++ vue/vovie/src/components/FoodItem.vue
@@ -0,0 +1,54 @@
+<template>
+    <div v-on:click="countClicks">
+        <h2>{{ name }}</h2>
+        <p>{{ message }}</p>
+        <p>clicked count: {{ clicks }}</p>
+        <h2>{{ foodName }}</h2>
+        <h3>{{ foodDesc }}</h3>
+        <button v-on:click="toggleFavorite">Favorite</button>
+        <img src="../assets/pawPink.png" alt="Pink Paw" v-show="foodIsFavorite" height="50px" width="50px">
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            name: 'Apples',
+            message: 'I like apples',
+            clicks: 0,
+            foodIsFavorite: this.isFavorite,
+        }
+    },
+    // props: [
+    //     'foodName',
+    //     'foodDesc',
+    //     'isFavorite',
+    // ],
+    props: {
+        foodName: {
+            type: String,
+            required: true,
+            validator: function (value) {
+                if (10 < value.length && value.length < 20) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        },
+        foodDesc: {type:String, required: false, default: 'this is food desc default value'},
+        isFavorite: Boolean,
+    },
+    methods: {
+        countClicks() {
+            this.clicks++;
+        },
+        toggleFavorite() {
+            this.foodIsFavorite = !this.foodIsFavorite;
+        }
+    }
+};
+</script>
+
+<style></style>
